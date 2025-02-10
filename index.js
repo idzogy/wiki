e68bc59b9e34b2ad227cf90b705e3103a1d67ca4
1010 <134185805+jedenzero@users.noreply.github.com>
2025-02-11 01:16:49 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const contentBox = document.getElementById('content');
 async function setContent(){
   const response = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/docs/${title}.md`);
   content = await response.text();
-  content = `<h1>${title}</h1>` + marked.parse(content);
+  content = marked.parse(content);
+  content = content.replace(/(?<=[^\!])\[\[([^\[\]]+)\]\]/g, `<a href="?title=$1">$1</a>`);
+  content = content.replace(/\!\[\[([^\[\]]+)\]\]/g, `<img src="/imgs/$1">`);
+  content = `<h1>${title}</h1>` + content;
   contentBox.innerHTML = content;
 }
 
