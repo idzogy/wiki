09882db10b05df052c7edd5d1b3109799dbdb975
1010 <134185805+jedenzero@users.noreply.github.com>
2025-02-15 09:11:48 -0500

    index.js 업데이트
    될까

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const title = new URL(window.location.href).searchParams.get('title');
 let content = '';
+
 const contentBox = document.getElementById('content');
 
 async function setContent(){
-    const response = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/docs/${title}.md`);
-    content = await response.text();
+    content = await getContent(title);
+    content = await readTemplates(content);
     content = marked.parse(content);
     content = content.replace(/(?<=[^\!])\[\[([^\[\]]+)\]\]/g, `<a href="?title=$1">$1</a>`);
     content = content.replace(/\!\[\[([^\[\]]+)\]\]/g, `<img src="imgs/$1">`);
@@ -12,4 +13,20 @@ async function setContent(){
     contentBox.innerHTML = content;
 }
 
-setContent();
+async function getContent(str){
+    const response = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/docs/${str}.md`);
+    return await response.text();
+}
+
+
+async function readTemplates(str) {
+    const reg = /\{\{(틀:[^{}]+)\}\}/;
+    contentBox.innerHTML+='reading started';
+    while(str.match(reg)) {
+        contentBox.innerHTML+=`match:${str.match(reg)[0]}`;
+        const replacing = await getContent(str.match(reg)[1]);
+        str = str.replace(str.match(reg)[0], replacing);
+    }
+
+    return str;
+}
