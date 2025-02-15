d5fa5ca4d59040f7f566bd0813cb8d915a970803
1010 <134185805+jedenzero@users.noreply.github.com>
2025-02-15 02:08:14 -0500

    index.js 업데이트

--- index.js
+++ index.js
@@ -3,14 +3,19 @@ let content = '';
 
 const contentBox = document.getElementById('content');
 
-async function setContent(){
-  const response = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/docs/${title}.md`);
-  content = await response.text();
-  content = marked.parse(content);
-  content = content.replace(/(?<=[^\!])\[\[([^\[\]]+)\]\]/g, `<a href="?title=$1">$1</a>`);
-  content = content.replace(/\!\[\[([^\[\]]+)\]\]/g, `<img src="imgs/$1">`);
-  content = `<h1>${title}</h1>` + content;
-  contentBox.innerHTML = content;
+function setContent(){
+    content = getContent(title);
+    content = content.replace(/\{\{(틀:[^{}]+)\}\}/g, (match, str) => getContent(str));
+    content = marked.parse(content);
+    content = content.replace(/(?<=[^\!])\[\[([^\[\]]+)\]\]/g, `<a href="?title=$1">$1</a>`);
+    content = content.replace(/\!\[\[([^\[\]]+)\]\]/g, `<img src="imgs/$1">`);
+    content = `<h1>${title}</h1>` + content;
+    contentBox.innerHTML = content;
+}
+
+async function getContent(str){
+    const response = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/docs/${str}.md`);
+    return await response.text();
 }
 
 setContent();
