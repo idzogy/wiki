226cd5feb4a7ff3317c984db75da5ace6ed7d4dc
1010 <134185805+jedenzero@users.noreply.github.com>
2025-02-15 09:32:20 -0500

    index.js 업데이트

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const title = new URL(window.location.href).searchParams.get('title');
 let content = '';
-
 const contentBox = document.getElementById('content');
+const reg = /\{\{(틀:[^{}]+)\}\}/;
 
 async function setContent(){
-    content = await getContent(title);
-    content = await readTemplates(content);
+    const response1 = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/docs/${title}.md`);
+    content = await response1.text();
+    
+    // templates
+    while(reg.test(content)){
+        const match = content.match(reg)[1];
+        const response2 = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/docs/${match}.md`);
+        const replacing = await response2.text();
+        
+        content = content.replace(reg, replacing)
+    }
+    
     content = marked.parse(content);
     content = content.replace(/(?<=[^\!])\[\[([^\[\]]+)\]\]/g, `<a href="?title=$1">$1</a>`);
     content = content.replace(/\!\[\[([^\[\]]+)\]\]/g, `<img src="imgs/$1">`);
@@ -13,20 +23,4 @@ async function setContent(){
     contentBox.innerHTML = content;
 }
 
-async function getContent(str){
-    const response = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/docs/${str}.md`);
-    return await response.text();
-}
-
-
-async function readTemplates(str) {
-    const reg = /\{\{(틀:[^{}]+)\}\}/;
-    contentBox.innerHTML+='reading started';
-    while(str.match(reg)) {
-        contentBox.innerHTML+=`match:${str.match(reg)[0]}`;
-        const replacing = await getContent(str.match(reg)[1]);
-        str = str.replace(str.match(reg)[0], replacing);
-    }
-
-    return str;
-}
+setContent();
