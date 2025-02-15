c9f527aa68ff4874c09c631e60d5e1301fef8d7c
1010 <134185805+jedenzero@users.noreply.github.com>
2025-02-15 04:33:25 -0500

    index.js 업데이트

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const contentBox = document.getElementById('content');
 
 async function setContent(){
     content = await getContent(title);
-    content = await readTemplates(content);
     content = marked.parse(content);
     content = content.replace(/(?<=[^\!])\[\[([^\[\]]+)\]\]/g, `<a href="?title=$1">$1</a>`);
     content = content.replace(/\!\[\[([^\[\]]+)\]\]/g, `<img src="imgs/$1">`);
@@ -14,22 +13,8 @@ async function setContent(){
 }
 
 async function getContent(str){
-    contentBox.innerHTML+=`${str}`;
     const response = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/docs/${str}.md`);
     return await response.text();
-}
-
-
-async function readTemplates(str) {
-    const reg = /\{\{(틀:[^{}]+)\}\}/;
-    contentBox.innerHTML+='reading started';
-    while(str.match(reg)) {
-        contentBox.innerHTML+=`match:${str.match(reg)[0]}`;
-        const replacing = await getContent(str.match(reg)[1]);
-        str = str.replace(str.match(reg)[0], replacing);
-    }
-    
-    return str;
 }
 
 setContent();
