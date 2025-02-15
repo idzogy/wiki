773b99dd3ad62db2839e7edaf33e87ffc9d4d817
1010 <134185805+jedenzero@users.noreply.github.com>
2025-02-15 05:39:00 -0500

    index.js 업데이트

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 const title = new URL(window.location.href).searchParams.get('title');
 let content = '';
-
 const contentBox = document.getElementById('content');
 
 async function setContent(){
-    content = await getContent(title);
+    const response = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/docs/${title}.md`);
+    content = await response.text();
     content = marked.parse(content);
     content = content.replace(/(?<=[^\!])\[\[([^\[\]]+)\]\]/g, `<a href="?title=$1">$1</a>`);
     content = content.replace(/\!\[\[([^\[\]]+)\]\]/g, `<img src="imgs/$1">`);
     content = `<h1>${title}</h1>` + content;
     contentBox.innerHTML = content;
-}
-
-async function getContent(str){
-    const response = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/docs/${str}.md`);
-    const i = await response.text();
-    return i;
 }
 
 setContent();
