65c3755f4f04475b6ab4001a727f1cecd91e25ee
1010 <134185805+jedenzero@users.noreply.github.com>
2025-02-21 16:57:26 -0500

    index.js 업데이트

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const title = new URL(window.location.href).searchParams.get('title');
 let content = '';
+let documents = [];
+const resultBox = document.getElementById('result');
 const contentBox = document.getElementById('content');
 const temp = /\{\{(틀:[^{}]+)\}\}/;
 const tempVar = /<<([^<>]+)>>/;
@@ -41,4 +43,33 @@ async function setContent(){
     contentBox.innerHTML = content;
 }
 
+async function setDocuments(){
+    const response = await fetch(`https://raw.githubusercontent.com/idzogy/wiki/main/assets/tree.json`);
+    documents = await response.json()[0]['contents'].map(doc => doc['name'].slice(0,-3));
+}
+
+function search(s){
+    resultBox.innerHTML = '';
+    documents.filter(row => row.includes(s)).sort((a,b) => sort(a,b,s)).slice(0,8).forEach(docName => {
+        resultBox.innerHTML = `<div><a href="?title=${docName}" style="color:var(--text);">${docName}</a></div>`;
+    });
+}
+
+function sort(a,b,s){
+    if(a.startsWith(s) && !b.startsWith(s)){
+        return -1;
+    }
+    if(!a.startsWith(s) && b.startsWith(s)){
+        return 1;
+    }
+    if(a.length > b.length){
+        return -1;
+    }
+    if(a.length < b.length){
+        return 1;
+    }
+    return 0;
+}
+
 setContent();
+setDocuments();
